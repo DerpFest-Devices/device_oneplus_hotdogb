4e53726a6252b3c4d2e8c727e65637fa103935b2
Anirudh Gupta <anirudhgupta109@gmail.com>
2019-12-11 11:06:56 +0000

    hotdogb: Build NoCutoutOverlay
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>
    Signed-off-by: Anirudh Gupta <anirudhgupta109@aosip.dev>

--- device.mk
+++ device.mk
@@ -47,6 +47,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom
 
+# Notch hide
+PRODUCT_PACKAGES += \
+    NoCutoutOverlay
+
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.2-service.oneplus_msmnile
